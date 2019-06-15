c522bcbd60c5953a715cbf46d41213a62cf3ae30
werleycordeiro <werleycordeiro@gmail.com>
2019-06-15 16:59:44 -0300

    from Rs

---
+++ Svensson.factor.loadings.R
@@ -0,0 +1,14 @@
+Z <- function(para,m)
+{
+  l1   <- para[1]
+  l2   <- para[2]  
+  
+  column1 <- rep.int(1,length(m))
+  column2 <- (1 / (l1 * m)) * exp(-l1 * m) * (exp(l1 * m) - 1)
+  column3 <- (1 / (l1 * m)) * exp(-l1 * m) * (-l1 * m + exp(l1 * m) - 1)
+  column4 <- (1 / (l2 * m)) * exp(-l2 * m) * (-l2 * m + exp(l2 * m) - 1)
+  
+  lambmat <- cbind(column1,column2,column3,column4)
+  
+  lambmat
+}  
