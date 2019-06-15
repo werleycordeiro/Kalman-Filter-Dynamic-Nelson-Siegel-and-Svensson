e78bc725d41790b524b3705ebea505baaea8a8a6
werleycordeiro <werleycordeiro@gmail.com>
2019-06-15 11:28:11 -0300

    from Rs

--- Kfilter.R
+++ Kfilter.R
@@ -1,5 +1,5 @@
 # Filter
-Kfilter<-function(logLik,N,T,Y,Z,a.t,P.t,H,a.tt,P.tt,v2,phi,mu,Q,prev,M,Yf){
+Kfilter<-function(N,T,Y,Z,a.t,P.t,H,a.tt,P.tt,v2,phi,mu,Q,prev,M,Yf){
 for (t in 1:T) 
 {
   v <- (as.numeric(Y[t, ])) - Z %*% a.t[t, ] # prediciton error vector
