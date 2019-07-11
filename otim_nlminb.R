33e9b8a15c8da041fd7c3a183aebd960ccc4274b
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-07-11 10:41:18 -0300

    Update otim_nlminb.R

--- otim_nlminb.R
+++ otim_nlminb.R
@@ -1,6 +1,6 @@
 otim_nlminb = function(para,Y,lik,prev,ahead,matu,lower,upper){
   source("DNS-baseline.R")
-  otim = nlminb(para,kalman,Y=data,lik=lik,prev=prev,ahead=ahead,matu=matu,control = list(trace=1,iter.max=50000),lower=low,upper=up)
-  return(otim)
+  nlminb(para,kalman,Y=data,lik=lik,prev=prev,ahead=ahead,matu=matu,control = list(trace=1,iter.max=50000),lower=low,upper=up)
 }
-otim_nlminb(para=para,Y=data,lik=lik,prev=prev,ahead=ahead,matu=matu,lower=low,upper=up)
+otim = otim_nlminb(para=para,Y=data,lik=lik,prev=prev,ahead=ahead,matu=matu,lower=low,upper=up)
+return(otim)
